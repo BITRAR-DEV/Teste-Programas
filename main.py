7c277665cc2b3d8cb02fdb0cbd8e0cf496818a24
BITRARofc <samucaps4b@gmail.com>
2026-08-29 22:12:00 -0300

    Funciona Mais ou Menos

--- main.py
+++ main.py
@@ -1,33 +1,73 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+from ultralytics import YOLO
 from picamera2 import Picamera2
 import cv2
 
-
 app = Flask(__name__)
+
+model = YOLO("mira_exp019.pt")
 
 picam2 = Picamera2()
 
 config = picam2.create_preview_configuration(
-    main={"format": "RGB888", "size": (1280, 720)}
+    main={"format": "RGB888", "size": (1920, 1080)}
 )
 
 picam2.configure(config)
-picam2.start()
 
-while True:
-    frame = picam2.capture_array()
-    cv2.imshow("Frame", frame)
+camera_ligada = False
 
-    if cv2.waitKey(1) == 27:
-        break
+def generate_frames():
+    global camera_ligada
+    while camera_ligada:
+        frame = picam2.capture_array()
 
+        results = model(frame, verbose=False)
 
+        frame = results[0].plot()
 
+        sucess, buffer = cv2.imencode(
+            ".jpg",
+            frame,
+            [cv2.IMWRITE_JPEG_QUALITY, 95]
+        )
 
+        if not sucess:
+            continue
+
+        frame = buffer.tobytes()
+        yield (b"--frame\r\n"
+               b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
 
 @app.route("/")
 def homepage():
     return render_template("index.html")
+
+@app.route("/iniciar-camera")
+def iniciar_camera():
+    global camera_ligada
+
+    if not camera_ligada:
+        picam2.start()
+        camera_ligada = True
+
+    return "ok"
+
+
+@app.route("/parar-camera")
+def parar_camera():
+    global camera_ligada
+
+    camera_ligada = False
+
+    if picam2.started:
+        picam2.stop()
+
+    return "ok"
+
+@app.route("/foto")
+def camera():
+   return Response(generate_frames(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 if __name__ == "__main__":
     app.run()
--- static/script.js
+++ static/script.js
@@ -1,11 +1,29 @@
 const modal = document.getElementById("cameraDialog");
 const abrir = document.getElementById("btnAbrir");
 const fechar = document.getElementById("btnFechar");
+const pontos = document.getElementById("pontos");
+const cameraImage = document.getElementById("cameraImage");
 
-abrir.addEventListener("click", () => {
+let quantidade = 0;
+
+setInterval(() => {
+    quantidade++;
+
+    if (quantidade > 3) {
+        quantidade = 0;
+    }
+
+    pontos.textContent = ".".repeat(quantidade);
+}, 500);
+
+abrir.addEventListener("click", async () => {
+  await fetch("/iniciar-camera");
+  cameraImage.src = "/foto?t=" + Date.now();
   modal.showModal(); 
 });
 
-fechar.addEventListener("click", () => {
+fechar.addEventListener("click", async () => {
+  cameraImage.src = "";
+  await fetch("/parar-camera");
   modal.close(); 
 });
